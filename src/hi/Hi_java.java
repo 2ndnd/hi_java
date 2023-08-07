50e93d78c2553d779f024f1a199a8d2aedcb08c4
2ndnd <2ndnd@naver.com>
2023-08-07 12:16:22 +0900

    2222

--- src/hi/Hi_java.java
+++ src/hi/Hi_java.java
@@ -5,7 +5,7 @@ public class Hi_java {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("성공");
-		System.out.println("2차시도");
+		System.out.println("2차시도.");
 
 	}
 
